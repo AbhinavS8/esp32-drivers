076f5dd8621c4aa3ec340bb1049266ec7af011b8
abhi <abhinav.subr@gmail.com>
2026-04-17 18:40:15 +0530

    tested uart functionality in main.c

--- main/main.c
+++ main/main.c
@@ -1,19 +1,33 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "gpio_driver.h"
-
+#include "uart_driver.h"
 #define LED_PIN 2
 
 void app_main(void)
 {
     gpio_output_ena(LED_PIN);
 
+    uart_config_t config = {
+        .baud_rate = 115200,
+        .data_bits = 8,
+        .stop_bits = 1,
+        .parity = 0
+    };
+
+    uart_ena(&config);
+
     while (1)
     {
         gpio_set(LED_PIN);
         vTaskDelay(pdMS_TO_TICKS(2000));
-
+        
+        uart_write_byte('a');
+        
         gpio_clear(LED_PIN);
         vTaskDelay(pdMS_TO_TICKS(2000));
+
+
     }
+
 }
